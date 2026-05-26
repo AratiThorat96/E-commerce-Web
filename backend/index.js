a08b2277d40c26a2edde38e643c0fe0254deb73a
Arati Thorat <143689263+AratiThorat96@users.noreply.github.com>
2026-05-27 03:11:35 +0530

    Update CORS origin for backend API

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:["https://e-commerce-web-frontendtwo-s4ca.onrender.com","http://localhost:5174"],
+    origin:["https://e-commerce-web-frontendtwo-s4ca.onrender.com","https://e-commerce-web-adminone.onrender.com"],
     credentials:true
 }))
 
